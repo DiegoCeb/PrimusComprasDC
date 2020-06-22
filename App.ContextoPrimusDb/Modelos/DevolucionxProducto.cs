fe2935f1080dcbc0e120838dfd2911cba33c1bf9
DiegoCeb <61908591+DiegoCeb@users.noreply.github.com>
2020-06-22 15:29:19 -0500

    Create DevolucionxProducto.cs

---
+++ App.ContextoPrimusDb/Modelos/DevolucionxProducto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.ContextoPrimusDb.Entidades
+{
+    public class DevolucionxProducto
+    {
+        public int Id { get; set; }
+
+        public int IdDevolucion { get; set; }
+
+        public int IdProducto { get; set; }
+    }
+}
