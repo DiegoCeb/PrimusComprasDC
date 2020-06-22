490a165f7e4b55fe2ea1fcd060e1a196761f7d88
DiegoCeb <61908591+DiegoCeb@users.noreply.github.com>
2020-06-22 15:29:10 -0500

    Create Bodegas.cs

---
+++ App.ContextoPrimusDb/Modelos/Bodegas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.ContextoPrimusDb.Entidades
+{
+    public class Bodegas
+    {
+        public int Id { get; set; }
+
+        public IList<Productos> IdProducto { get; set; }
+
+        public int Cantidad { get; set; }
+    }
+}
