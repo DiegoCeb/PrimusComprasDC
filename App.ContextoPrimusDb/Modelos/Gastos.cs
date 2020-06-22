6a6e642b006ddbda7192c6badeb30fc6f0624479
DiegoCeb <61908591+DiegoCeb@users.noreply.github.com>
2020-06-22 15:29:22 -0500

    Create Gastos.cs

---
+++ App.ContextoPrimusDb/Modelos/Gastos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.ContextoPrimusDb.Entidades
+{
+    public class Gastos
+    {
+        public int Id { get; set; }
+
+        public double Total { get; set; }
+    }
+}
