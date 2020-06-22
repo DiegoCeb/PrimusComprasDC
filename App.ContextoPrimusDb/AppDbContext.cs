be19b13f49a80a2154392f26761a3450fd04245d
DiegoCeb <61908591+DiegoCeb@users.noreply.github.com>
2020-06-22 15:28:34 -0500

    Create AppDbContext.cs

---
+++ App.ContextoPrimusDb/AppDbContext.cs
@@ -0,0 +1,43 @@
+﻿using App.ContextoPrimusDb.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.ContextoPrimusDb
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Productos> Productos { get; set; }
+
+        public DbSet<Bodegas> Bodegas { get; set; }
+
+        public DbSet<Categorias> Categorias { get; set; }
+
+        public DbSet<Devoluciones> Devoluciones { get; set; }
+
+        public DbSet<Gastos> Gastos { get; set; }
+
+        public DbSet<Proveedores> Proveedores { get; set; }
+
+        public DbSet<Marcas> Marcas { get; set; }
+
+        public DbSet<Usuarios> Usuarios { get; set; }
+
+        public DbSet<Ventas> Ventas { get; set; }
+
+        public DbSet<DevolucionxProducto> DevolucionxProducto { get; set; }
+
+        public DbSet<GastosxProductos> GastosxProductos { get; set; }
+
+        public DbSet<ProveedoresxProducto> ProveedoresxProducto { get; set; }
+
+        public DbSet<VentasxProductos> VentasxProductos { get; set; }
+
+    }
+}
