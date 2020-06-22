5e5a2f9484e44cd0f0857fcf6945418d3a996740
DiegoCeb <61908591+DiegoCeb@users.noreply.github.com>
2020-06-22 15:29:24 -0500

    Create GastosxProductos.cs

---
+++ App.ContextoPrimusDb/Modelos/GastosxProductos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.ContextoPrimusDb.Entidades
+{
+    public class GastosxProductos
+    {
+        public int Id { get; set; }
+
+        public int idProducto { get; set; }
+
+        public int idGastos { get; set; }
+    }
+}
