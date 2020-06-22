2db7c7d6c8cb34445421c5cf4a0c1376f7a606da
DiegoCeb <61908591+DiegoCeb@users.noreply.github.com>
2020-06-22 15:29:16 -0500

    Create Devoluciones.cs

---
+++ App.ContextoPrimusDb/Modelos/Devoluciones.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.ContextoPrimusDb.Entidades
+{
+    public class Devoluciones
+    {
+        public int Id { get; set; }
+
+        public string MotivoDevolucion { get; set; }
+    }
+}
