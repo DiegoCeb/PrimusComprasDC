be7cdc48da54728e9480e3d7c1dbb693d54f1726
DiegoCeb <61908591+DiegoCeb@users.noreply.github.com>
2020-06-22 15:29:13 -0500

    Create Categorias.cs

---
+++ App.ContextoPrimusDb/Modelos/Categorias.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.ContextoPrimusDb.Entidades
+{
+    public class Categorias
+    {
+        public int Id { get; set; }
+
+        public string NombreCategoria { get; set; }
+
+    }
+}
